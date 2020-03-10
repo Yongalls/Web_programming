efb39a3b78a32275b8c31737b3aa8631f7a7d22c
Yongalls <lym7505@kaist.ac.kr>
2020-03-10 11:56:38 +0900

    FIX #4: made it impossible to move plates below another plate

--- hanoi_game/script.js
+++ hanoi_game/script.js
@@ -17,7 +17,7 @@ var x_p, y_p; // position of the cursor relative to the plate
 var x_0, y_0; // position of the plate relative to the document
               // right after 'mousedown' event
 
-// values
+// values (px)
 var gap_rods = 315;
 var width_rod = 45;
 var hegiht_rod = 335;
@@ -42,6 +42,9 @@ var rod3 = {
   dragend: [855,900,200,535]
 }
 
+var rods = [rod1,rod2,rod3];
+var cur_rod;
+
 function get_pos(event,obj){
   document.getElementById('dev').innerHTML = "(" + event.clientX +"," + event.clientY + ")";
 }
@@ -52,9 +55,26 @@ function onMousedown(event,plate){
   y_p = event.offsetY;
   x_0 = cur_plate.style.left.slice(0,-2);
   y_0 = cur_plate.style.top.slice(0,-2);
-  document.onmousemove = onMousemove;
-  document.onmouseup = onMouseup;
-  cur_plate.innerHTML = x_0 + "," + y_0;
+
+  var x_0_cen = parseInt(x_0) + parseInt(cur_plate.style.width.slice(0,-2))/2;
+  var y_0_cen = parseInt(y_0) + parseInt(cur_plate.style.height.slice(0,-2))/2;
+
+  //finding current rod
+  for (i in rods){
+    if(x_0_cen > rods[i].dragend[0] && x_0_cen < rods[i].dragend[1]
+      && y_0_cen > rods[i].dragend[2] && y_0_cen < rods[i].dragend[3]){
+        cur_rod = rods[i];
+        break;
+    }
+  }
+
+  //check if cur_plate is above the rest of plates
+  var id_plate = cur_plate.id.split('_')[1];
+  if (id_plate == cur_rod.plates[cur_rod.plates.length - 1]){
+    document.onmousemove = onMousemove;
+    document.onmouseup = onMouseup;
+    cur_plate.innerHTML = cur_rod.id;
+  }
 }
 
 function onMousemove(event){
@@ -69,29 +89,12 @@ function onMouseup(event){
   var pos_x = event.clientX - x_p + parseInt(cur_plate.style.width.slice(0,-2))/2; //pos of center
   var pos_y = event.clientY - y_p + parseInt(cur_plate.style.height.slice(0,-2))/2;
 
-  cur_plate.innerHTML = "up";
-
-  var rods = [rod1,rod2,rod3];
-  var x_0_cen = parseInt(x_0) + parseInt(cur_plate.style.width.slice(0,-2))/2;
-  var y_0_cen = parseInt(y_0) + parseInt(cur_plate.style.height.slice(0,-2))/2;
-
-  var cur_rod;
-  for (i in rods){
-    console.log(i);
-    if(x_0_cen > rods[i].dragend[0] && x_0_cen < rods[i].dragend[1]
-      && y_0_cen > rods[i].dragend[2] && y_0_cen < rods[i].dragend[3]){
-        console.log("dele");
-        cur_rod = rods[i];
-        rods.splice(i,1);
-        break;
-    }
-  }
-
   var correct = 0;
 
-  console.log(rods.length);
-
   for (rod of rods){
+    if (cur_rod.id == rod.id){
+      continue;
+    }
     if(pos_x > rod.dragend[0] && pos_x < rod.dragend[1] && pos_y > rod.dragend[2] && pos_y < rod.dragend[3]){
       console.log(rod.plates[rod.plates.length - 1]);
       console.log(cur_rod.plates[cur_rod.plates.length - 1]);
@@ -116,9 +119,7 @@ function onMouseup(event){
     cur_plate.style.top = y_0 + "px";
   }
 
-  console.log(rod1);
-  console.log(rod2);
-  console.log(rod3);
+  console.log(cur_plate.id);
 
   count += correct;
   document.getElementById('count').innerHTML = "Total count: " + count;
